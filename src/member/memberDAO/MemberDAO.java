ef7a70df040637cbfa11332768e21b539a220201
PuppyWinnie <dadda8494772@gmail.com>
2022-05-18 10:41:58 +0900

    Connection Pool  설정 및 DAO
    우선 DBConn 은  제외하고 작업하셔도 됩니다

--- src/member/memberDAO/MemberDAO.java
+++ src/member/memberDAO/MemberDAO.java
@@ -1,5 +1,183 @@
 package member.memberDAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import member.memberVO.MemberVO;
+//import member.memberDBConn.MemberDBConn;
+
 public class MemberDAO {
+	
+	private static Connection con;
+	private PreparedStatement ps;
+	private ResultSet rs;
+	
+	
+	public MemberDAO() {}
+	
+	private static MemberDAO instance = new MemberDAO();
+	// = new MemberDAO()
+	// MemberDAO 클래스도 계속 지속적으로 사용해야 하므로 
+	// static 객체를 생성하고 호출시에는 instance 를 호출하면 됩니다. 
+	
+	public static MemberDAO getInstance() {
+		if(instance == null) {
+			instance = new MemberDAO();
+		}
+		return instance;
+	}
+	
+	private static DataSource ds;
+	
+	static {
+		try {
+
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+			ds = (DataSource)envCtx.lookup("jdbc/oracle");
+			con = ds.getConnection();
+		}catch(NamingException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	// DAO 작성 예시 ( 전체 회원 목록 출력 )
+	public List<MemberVO> selectAll(){
+		
+		ArrayList<MemberVO> list = new ArrayList<MemberVO>();
+		
+		String sql = "select * from member"; 
+		
+		try {
+			con = ds.getConnection();
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				MemberVO vo = new MemberVO();
+				vo.setNo(rs.getInt("no"));
+				vo.setId(rs.getString("id"));
+				vo.setPassword(rs.getString("password"));
+				vo.setName(rs.getString("name"));
+				vo.setTel1(rs.getString("tel1"));
+				vo.setTel2(rs.getString("tel2"));
+				vo.setTel3(rs.getString("tel3"));
+				list.add(vo);
+			}
+			
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs != null) rs.close();
+				if(ps != null) ps.close();
+				if(con != null) con.close();
+					
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		if(list.isEmpty()) {
+			list = null;
+		}else {
+			list.trimToSize();
+		}
+		
+		return list;
+	}
+	
+	
+	// 회원 가입 
+	
+	public boolean insertJoin(MemberVO vo) {
+		
+		boolean check = false;
+		
+		String sql = "insert into member values(member_seq.nextval,?,?,?,?,?,?)";
+		
+		try {
+			con = ds.getConnection();
+			ps = con.prepareStatement(sql);
+			ps.setString(1, vo.getId());
+			ps.setString(2, vo.getPassword());
+			ps.setString(3, vo.getName());
+			ps.setString(5, vo.getTel1());
+			ps.setString(6, vo.getTel2());
+			ps.setString(7, vo.getTel3());
+			if(ps.executeUpdate() != 0) {
+				check = true;  
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(ps != null) ps.close();
+				if(con != null) con.close();
+			}catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		 
+		return check;
+		// check가 true 로 리턴 -> 회원가입 성공
+		// false 로 리턴 ->회원가입 실패 
+	}
+	
+	
+	// 로그인 확인
+	public MemberVO checkLogin(MemberVO vo) {
+		
+		String sql = "select * from member where id=?, password=?";
+		
+		try {
+			con = ds.getConnection();
+			ps = con.prepareStatement(sql);
+			ps.setString(1, "id");
+			ps.setString(2, "pw");
+			rs = ps.executeQuery();
+			vo = selectMember(rs); 
+		}catch(SQLException e) {
+			System.out.println("login 실패");
+			
+		}finally {
+			try {
+				if(ps != null) ps.close();
+				if(con != null) con.close();
+			}catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return vo;  
+	}
+	
+	private MemberVO selectMember(ResultSet rs) {
+		
+		MemberVO vo = null;
+		
+		try {
+			if(rs.next()) {
+				vo = new MemberVO();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return vo;
+	}
+	
+	
+	
+	
 
 }
--- src/member/memberDBConn/MemberDBConn.java
+++ src/member/memberDBConn/MemberDBConn.java
@@ -1,5 +1,36 @@
 package member.memberDBConn;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class MemberDBConn {
+	
+	private static DataSource ds;
+	private static Context context;
+	private static Connection con;
+	
+	// Connection Pool 을 이용한 DB Connect
+	public static Connection getConeection() {
+		
+		try {
+			context = new InitialContext();
+//			ds = (DataSource) context.lookup("java:comp/env/jdbc/oracle");
+//			con = ds.getConnection();
+			context = (Context) context.lookup("java:/comp/env");
+			ds = (DataSource)context.lookup("jdbc/oracle");
+			con = ds.getConnection();
+		}catch(NamingException e) {
+			e.printStackTrace();
+		}catch(SQLException e1) {
+			e1.printStackTrace();
+		}
+
+		return con;
+	}
 
 }
