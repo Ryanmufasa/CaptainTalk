2ebc9f6602a9488ae403b33da4a33094f12b7d8f
Yoooong <92244572+YoooonJJ@users.noreply.github.com>
2022-05-20 16:21:21 +0900

    FollowDAO 회원 팔로워, 팔로잉 목록 조회 기능 추가

--- src/follow/followDAO/FollowDAO.java
+++ src/follow/followDAO/FollowDAO.java
@@ -87,13 +87,97 @@ public class FollowDAO {
 	}
 	
 	
-	// follow 테이블에서 기준이 되는 회원 번호는 target_mem_no 입니다.
-	// target_mem_no 의 기준으로 팔로잉, 팔로워를 조회해야 합니다. 
+	// follow 테이블에서 회원의 팔로워 조회시 기준이 되는 회원 번호는 target_mem_no 입니다.
+	// 회원의 팔로워 목록 조회 ( 팔로워의 아이디, 이름, 날짜 )
+	public List<Object> myfollower(FollowVO vo){
+		
+		ArrayList<Object> list = new ArrayList<Object>();
+		
+		String sql = "select m.mem_id, m.mem_name, f.fol_date " 
+				+ "from member m, follow f "
+				+ "where f.target_mem_no = ? "
+				+ "and m.mem_no = f.target_mem_no";
+		
+		try {
+			con = ds.getConnection();
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, vo.getTarget_mem_no());
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				Map<String,String> fr = new HashMap<>();
+				// while문 밖에서 선언하면 key가 동일하기 때문에 value만 바뀝니다
+				fr.put("mem_id", rs.getString("m.mem_id"));
+				fr.put("mem_name", rs.getString("m.mem_name"));
+				fr.put("fol_date", (rs.getDate("f.fol_date")).toString());
+				list.add(fr);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs!=null)rs.close();
+				if(ps!=null) ps.close();
+				if(con!=null) con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if(list.isEmpty()) {
+			list = null;
+		}else {
+			list.trimToSize();
+		}
+		
+		return list;
+		// list 에 들어간 데이터는 <key,value> 형태의 HashMap 입니다
+	}
 	
-	// 회원의 팔로워 조회 ( 팔로워의 아이디, 이름, 날짜 )
-	//public ArrayList<String> 
-	
-	
+	// follow 테이블에서 회원의 팔로잉 목록 조회시 기준이 되는 회원 번호는 mem_no 입니다.
+	// 회원의 팔로잉 목록 조회
+	public List<Object> myfollowing(FollowVO vo){
+		
+		ArrayList<Object> list = new ArrayList<Object>();
+		
+		String sql = "select m.mem_id, m.mem_name, f.fol_date " 
+				+ "from member m, follow f "
+				+ "where f.mem_no = ? "
+				+ "and m.mem_no = f.target_mem_no";
+		
+		try {
+			con = ds.getConnection();
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, vo.getMem_no());
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				Map<String,String> fr = new HashMap<>();
+				// while문 밖에서 선언하면 key가 동일하기 때문에 value만 바뀝니다
+				fr.put("mem_id", rs.getString("m.mem_id"));
+				fr.put("mem_name", rs.getString("m.mem_name"));
+				fr.put("fol_date", (rs.getDate("f.fol_date")).toString());
+				list.add(fr);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs!=null)rs.close();
+				if(ps!=null) ps.close();
+				if(con!=null) con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if(list.isEmpty()) {
+			list = null;
+		}else {
+			list.trimToSize();
+		}
+		
+		return list;
+		// list 에 들어간 데이터는 <key,value> 형태의 HashMap 입니다
+	}
 	
 
 }
