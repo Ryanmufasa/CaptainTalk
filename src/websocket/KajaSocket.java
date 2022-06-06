739b6624f295470fe2f0fade4a5f369f58b176e7
rlxo4307 <105694582+rlxo4307@users.noreply.github.com>
2022-06-06 19:20:32 +0900

    임시커밋

--- src/websocket/KajaSocket.java
+++ src/websocket/KajaSocket.java
@@ -24,17 +24,17 @@ import member.memberVO.MemberVO;
 @ServerEndpoint(value = "/kaja", configurator = HttpSessionConfigurator.class) // 클라이언트는 이 주소로 서버에 접속한다.  // 서버에 접속하는 위치가 /kaja 라는 것. 
 public class KajaSocket {
 	
-	private Map<Session, EndpointConfig> listclient = Collections.synchronizedMap(new HashMap<>());
+	private static List<Session> listclient  = Collections.synchronizedList(new ArrayList<>());
 	
-	private static List<Session> listclient2  = Collections.synchronizedList(new ArrayList<>());
+	private Map<Session, EndpointConfig> listclient2 = Collections.synchronizedMap(new HashMap<>());
 	
 	
 	@OnOpen //  client가 접속하면  
 	public void onOpen(Session userSession, EndpointConfig config) {
-		if (!listclient.containsKey(userSession)) {
-			listclient.put(userSession, config);
+		listclient.add(userSession);
+		if (!listclient2.containsKey(userSession)) {
+			listclient2.put(userSession, config);
 		}
-		listclient2.add(userSession);
 	} // 홍길동 소켓을 세션목록에 넣는다. 
 	
 	// 어노테이션의 장점 -> 코드를 짧게 쓰면서도 많은 걸 포함한 기능 구현을 가능하게 한다. 
@@ -48,7 +48,6 @@ public class KajaSocket {
 		String[] wantName = message.split("]");
 		int toName = wantName[0].length();
 		String name = wantName[0].substring(1,toName);
-		System.out.println("넘어온 chat으로부터의 Name : " + name);
 		
 		//메시지를 보낸 사용자가 들어있는 방을 구한다
 		String room = cao.getRoom(name);
@@ -56,25 +55,32 @@ public class KajaSocket {
 		//메시지를 전달한 사용자가 들어있는 방의 사용자들을 리스트로 구한다
 		ArrayList<String> ids = cao.getMemIds(room);
 
-		synchronized (listclient2) {
-			for(Session imsi : listclient2) {
-				for(String member : ids) {
+		//메시지를 전달한 사용자와 같은 방에 있는 사용자의 세션에 메시지를 보낸다
+		synchronized (listclient) {
+			for(Session imsi : listclient) { //test01, test02, test03, ...
+				for(String member : ids) { // ids : { rlxo, test01 }
 					System.out.println("member : " + member);
 							
-					if (listclient.containsKey(imsi)) {      
-						EndpointConfig config = listclient.get(imsi);
-								
-						// HttpSessionConfigurator에서 설정한 session값을 가져온다.	
-						HttpSession session = 
-								(HttpSession)config.getUserProperties().get(HttpSessionConfigurator.Session);
-								
-						//가져온 session으로부터 imsiName 변수에 사용자 이름을 담는다.
-						MemberVO vo = (MemberVO)session.getAttribute(member);
-						String imsiName = vo.getMem_id();
-		
-						if(!imsi.equals(userSession) && member == imsiName) {
-							imsi.getBasicRemote().sendText(message);
-						}
+					synchronized (listclient2) {
+						if (listclient2.containsKey(imsi)) { // listclient key contains test01, test02...
+							EndpointConfig config = listclient2.get(imsi);
+							// HttpSessionConfigurator에서 설정한 session값을 가져온다.	
+							HttpSession session = 
+									(HttpSession)config.getUserProperties().get(HttpSessionConfigurator.Session);
+							//vo객체에 room으로 부터 얻어온 list(member)의 세션을 담는다
+							MemberVO vo = (MemberVO)session.getAttribute(member);
+							System.out.println("체크 포인트");
+							
+							//imsi session의 사용자 Id를 imsiName에 저장한다
+							String imsiName = vo.getMem_id();
+							
+							System.out.println("imsiName(listclient2의 Session) : " + imsiName);
+							
+							//현재 세션을 제외하고 && (member:채팅방 내 사용자 ID) == (imsiName: imsi session의 사용자 ID)
+							if(!imsi.equals(userSession) && member == imsiName) {
+								imsi.getBasicRemote().sendText(message);
+							}
+						}	
 					}		
 				}	
 			}
